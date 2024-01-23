f7585480ba5ad749f00d1cc6d9a2c736b596f3e1
Nick Ramsey <nramseysc@gmail.com>
2024-01-23 11:01:32 -0700

    basic lemmatization

--- text-processing-pipeline.py
+++ text-processing-pipeline.py
@@ -1,4 +1,7 @@
+# Following along with chapter 2
 import spacy
+# Need to run $ python -m spacy download en in terminal
 nlp = spacy.load("en_core_web_sm")
-doc = nlp(u'I am flying to Frisco')
-print([w.text for w in doc])
+doc = nlp(u'this product integrates both libraries for downloading and applying patches')
+for token in doc:
+    print(token.text, token.lemma_)
