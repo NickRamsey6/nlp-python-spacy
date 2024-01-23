86ca5d091df704253b73ef85b8d2de3d7c4813d8
Nick Ramsey <nramseysc@gmail.com>
2024-01-23 11:28:52 -0700

    print part of speech and dependency

--- text-processing-pipeline.py
+++ text-processing-pipeline.py
@@ -4,6 +4,6 @@ import spacy
 # Need to run $ python -m spacy download en in terminal
 nlp = spacy.load("en_core_web_sm")
 doc = nlp(u'I have flown to LA. Now I am flying to Frisco')
-print([w.text for w in doc if w.tag_ == 'VBG' or w.tag_ =='VB'])
-print([w.text for w in doc if w.pos_ == 'PROPN'])
+for token in doc:
+    print(token.text, token.pos_, token.dep_)
 
