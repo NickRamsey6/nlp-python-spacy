c0330284f0f123b54720eb5b81ba9eeb455588f3
Nick Ramsey <nramseysc@gmail.com>
2024-01-23 11:21:58 -0700

    use part of speech tags to identify verbs in base or inifitive form and verbs in present progressive form also id propper nouns

--- text-processing-pipeline.py
+++ text-processing-pipeline.py
@@ -1,11 +1,9 @@
 # Following along with chapter 2
-import spacy
-from spacy.symbols import ORTH, LEMMA
-# Need to run $ python -m spacy download en in terminal
 # Had to downgrade to Spacy 2.3.5 b/c they recently changed tokenizer exceptions to ORTH and NORM
+import spacy
+# Need to run $ python -m spacy download en in terminal
 nlp = spacy.load("en_core_web_sm")
-doc = nlp(u'I am flying to Frisco')
-print([w.text for w in doc])
-special_case = [{ORTH: u'Frisco', LEMMA: u'San Francisco'}]
-nlp.tokenizer.add_special_case(u'Frisco', special_case)
-print([w.lemma_ for w in nlp(u'I am flying to Frisco')])
+doc = nlp(u'I have flown to LA. Now I am flying to Frisco')
+print([w.text for w in doc if w.tag_ == 'VBG' or w.tag_ =='VB'])
+print([w.text for w in doc if w.pos_ == 'PROPN'])
+
