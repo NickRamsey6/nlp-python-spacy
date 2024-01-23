2f72d80fb6476ccda9571e5074bdc0949cd35a87
Nick Ramsey <nramseysc@gmail.com>
2024-01-23 11:16:15 -0700

    create special case for tokenizer to apply when lemmatizing

--- text-processing-pipeline.py
+++ text-processing-pipeline.py
@@ -1,7 +1,11 @@
 # Following along with chapter 2
 import spacy
+from spacy.symbols import ORTH, LEMMA
 # Need to run $ python -m spacy download en in terminal
+# Had to downgrade to Spacy 2.3.5 b/c they recently changed tokenizer exceptions to ORTH and NORM
 nlp = spacy.load("en_core_web_sm")
-doc = nlp(u'this product integrates both libraries for downloading and applying patches')
-for token in doc:
-    print(token.text, token.lemma_)
+doc = nlp(u'I am flying to Frisco')
+print([w.text for w in doc])
+special_case = [{ORTH: u'Frisco', LEMMA: u'San Francisco'}]
+nlp.tokenizer.add_special_case(u'Frisco', special_case)
+print([w.lemma_ for w in nlp(u'I am flying to Frisco')])
