8deff69f78bb28df4852e8c6ad643360362e8372
Nick Ramsey <nramseysc@gmail.com>
2024-01-24 14:41:05 -0700

    combined script

--- text-processing-pipeline.py
+++ text-processing-pipeline.py
@@ -2,8 +2,9 @@
 # Had to downgrade to Spacy 2.3.5 b/c they recently changed tokenizer exceptions to ORTH and NORM
 import spacy
 # Need to run $ python -m spacy download en in terminal
+from spacy.symbols import ORTH, LEMMA
 nlp = spacy.load("en_core_web_sm")
 doc = nlp(u'I have flown to LA. Now I am flying to Frisco')
-for sent in doc.sents:
-    print([w.text for w in sent if w.dep_ == 'ROOT' or w.dep_ == 'pobj'])
-
+special_case = [{ORTH: u'Frisco', LEMMA: u'San Francisco'}]
+nlp.tokenizer.add_special_case(u'Frisco', special_case)
+print([w.lemma_ for w in nlp(u'I have flown to LA. Now I am flying to Frisco') if w.dep_=='ROOT' and w.tag_=='VBG' or w.dep_=='pobj' and ((w.head).head).tag_=='VBG'])
