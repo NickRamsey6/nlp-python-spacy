da979ef37848d00e47c79798f5e9ab7bd34089cc
Nick Ramsey <nramseysc@gmail.com>
2024-01-24 14:58:25 -0700

    access sentence container

--- container-objects-customizing-spacy.py
+++ container-objects-customizing-spacy.py
@@ -7,3 +7,9 @@ nlp = spacy.load("en_core_web_sm")
 # Obtain all leftward syntactic children of word at position 4
 doc = nlp(u'I want a green apple.')
 print([w for w in doc[4].lefts])
+# Could also use [w for w in doc[4].children] to get all children
+
+# Separate text into sentences and return all tokens in each individual sentence
+doc = nlp(u'A severe storm hit the beach. It started to rain.')
+for sent in doc.sents:
+    print([sent[i] for i in range(len(sent))])
