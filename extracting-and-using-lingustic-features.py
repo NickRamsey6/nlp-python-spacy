428210dcfc8fd8c193432561f6a408ac7f1cea99
Nick Ramsey <nramseysc@gmail.com>
2024-01-25 17:57:20 -0700

    turn statements into questions

--- extracting-and-using-lingustic-features.py
+++ extracting-and-using-lingustic-features.py
@@ -25,3 +25,31 @@ for token in doc:
         break
 phrase = phrase[:-1]
 print(phrase)
+
+# Turn statement into question
+doc = nlp(u"I can promise it is worth your time.")
+sent = ''
+for i, token in enumerate(doc):
+    if token.tag_ == 'PRP' and doc[i+1].tag_ == 'MD' and doc[i+2].tag_ == 'VB':
+        sent = doc[i+1].text.capitalize() + ' ' + doc[i].text
+        sent = sent + ' ' + doc[i+2:].text
+        break
+# Retokenization
+doc = nlp(sent)
+for i, token in enumerate(doc):
+    if token.tag_ == 'PRP' and token.text == 'I':
+        sent = doc[:i].text + ' you ' + doc[i+1:].text
+        break
+doc = nlp(sent)
+for i, token in enumerate(doc):
+    if token.tag_ == 'PRP$' and token.text == 'your':
+        sent = doc[:i].text + ' my ' + doc[i+1:].text
+        break
+doc = nlp(sent)
+for i, token in enumerate(doc):
+    if token.tag_ == 'VB':
+        sent = doc[:i].text + ' really ' + doc[i:].text
+        break
+doc = nlp(sent)
+sent = doc[:len(doc)-1].text + '?'
+print(sent)
