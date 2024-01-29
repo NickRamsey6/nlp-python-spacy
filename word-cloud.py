d0d8340755a30e82e3bfb1edfd6aff04e6a037d1
Nick Ramsey <nramseysc@gmail.com>
2024-01-29 12:10:37 -0700

    generate the word cloud

--- word-cloud.py
+++ word-cloud.py
@@ -16,3 +16,16 @@ stopwords.update(['us', 'one', 'will', 'said', 'now', 'well',
                   'man', 'may', 'little', 'say', 'must', 'way',
                   'long', 'yet', 'mean', 'put', 'seem', 'asked',
                   'made', 'half', 'much', 'certainly', 'might', 'came'])
+
+wc = WordCloud(max_words=500,
+               relative_scaling=0.5,
+               mask=mask,
+               background_color='white',
+               stopwords=stopwords,
+               margin=2,
+               random_state=7,
+               contour_width=2,
+               contour_color='brown',
+               colormap='copper').generate(text)
+
+colors = wc.to_array()
