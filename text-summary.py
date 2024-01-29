7a7d4d3df516b1c5475682634662a4f21762d0c7
Nick Ramsey <nramseysc@gmail.com>
2024-01-29 15:06:18 -0700

    code for typos in mlk speech and word frequencies

--- text-summary.py
+++ text-summary.py
@@ -13,3 +13,21 @@ def main():
     p_elems = [element.text for element in soup.find_all('p')]
 
     speech = ''.join(p_elems)
+
+    # Fix typo in mlk speech
+    speech = speech.replace(')mowing', 'knowing')
+    speech = re.sub('\s+', ' ', speech)
+    speech_edit = re.sub('[^a-zA-Z]', ' ', speech)
+    speech_edit = re.sub('\s+', ' ', speech_edit)
+
+    while True:
+        max_words = input("Enter max words per sentence for summary: ")
+        num_sents = input("Enter number of sentences for summary: ")
+        if max_words.isdigit() and num_sents.isdigit():
+            break
+        else:
+            print("\n Input must be whole numbers. \n")
+
+    speech_edit_no_stop = remove_stop_words(speech_edit)
+    word_freq = get_word_freq(speech_edit_no_stop)
+    sent_scores = score_sentences(speech, word_freq, max_words)
