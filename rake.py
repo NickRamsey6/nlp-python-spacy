03dc189e5568192dde549fdca91099e72158e314
Nick Ramsey <nramseysc@gmail.com>
2024-01-30 15:19:01 -0700

    extract text from hard coded url

--- rake.py
+++ rake.py
@@ -3,3 +3,11 @@ import nltk
 # nltk.download('stopwords')
 from requests_html import HTMLSession
 from rake_nltk import Rake
+
+def extract_text():
+    s = HTMLSession()
+    url = 'https://www.musicradar.com/reviews/tech/akg-c214-172209'
+    response = s.get(url)
+    return response.html.find('div#article-body', first=True).text
+
+print(extract_text())
